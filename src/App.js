5f050eec97dca1ba6e63d6c4e8679e84e83f4a35
Skye Ridge <mridge@csumb.edu>
2021-10-20 11:51:04 -0700

    moved the network sim to an external file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+var network = require('./networkSim');
 
 function App() {
 
+  //examples on how to use it
+  //call function whenever you want a value
   for(let i = 0; i < 10; i++){
-    console.log("Download " + i + " " + getDownloadSpeed())
+    console.log("Download " + i + " " + network.getDownloadSpeed())
   }
   for(let i = 0; i < 10; i++){
-    console.log("Upload " + i + " " + getUploadSpeed())
+    console.log("Upload " + i + " " + network.getUploadSpeed())
   }
   for(let i = 0; i < 10; i++){
-    console.log("Ping " + i + " " + getPing())
+    console.log("Ping " + i + " " + network.getPing())
   }
   for(let i = 0; i < 10; i++){
-    console.log("Upload " + i + " " + getJitter())
+    console.log("Upload " + i + " " + network.getJitter())
   }
   return (
     <div className="App">
@@ -35,104 +38,6 @@ function App() {
   );
 }
 
-// add stability and variable speeds
-// first variable is stability and second variable is speed
-// stability: 0 low variability - 1 high variability
-// speed: 0 being no connection - 1 being very fast
-function getDownloadSpeed(stability, speed){
-  if (stability === undefined){
-    stability = .1
-  }
-  if(speed === undefined){
-    speed = .1
-  }
 
-  var startSpeed = 1000
-
-  var min = startSpeed * speed;
-  min -= min * stability
-  console.log("min: " + min);
-  if(min < 0){
-    min = 0;
-  }
-
-  var max = startSpeed * speed;
-  max += max * stability;
-  console.log("max: " + max);
-
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getUploadSpeed(stability, speed){
-  if (stability === undefined){
-    stability = .1
-  }
-  if(speed === undefined){
-    speed = .1
-  }
-
-  var startSpeed = 1000
-
-  var min = startSpeed * speed;
-  min -= min * stability
-  console.log("min: " + min);
-  if(min <= 0){
-    min = 1;
-  }
-
-  var max = startSpeed * speed;
-  max += max * stability;
-  console.log("max: " + max);
-
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getPing(stability, speed){
-  if (stability === undefined){
-    stability = .3
-  }
-  if(speed === undefined){
-    speed = .2
-  }
-
-  var startSpeed = 300
-
-  var min = startSpeed * speed;
-  min -= min * stability
-  console.log("min: " + min);
-  if(min <= 0){
-    min = 1;
-  }
-
-  var max = startSpeed * speed;
-  max += max * stability;
-  console.log("max: " + max);
-
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getJitter(stability, speed){
-  if (stability === undefined){
-    stability = .1
-  }
-  if(speed === undefined){
-    speed = .5
-  }
-
-  var startSpeed = 60
-
-  var min = startSpeed * speed;
-  min -= min * stability
-  console.log("min: " + min);
-  if(min <= 0){
-    min = 1;
-  }
-
-  var max = startSpeed * speed;
-  max += max * stability;
-  console.log("max: " + max);
-
-  return Math.floor(Math.random() * (max - min) + min);
-}
 
 export default App;
