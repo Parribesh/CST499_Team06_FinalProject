f3118056cd3267698f0700050d396c2909373f0e
cestes-csumb <60244674+cestes-csumb@users.noreply.github.com>
2021-10-27 19:04:06 -0700

    Added Upload Test/Transition Method - Download test runs and then transitions to upload test via delay (see comment) - Label changes once download test finishes to "Upload Test" - TODO: - Change rate at which speedometer is updated - Narrow range of generated values (to mimic a real test) - Drop speedometer back down to 0 after test - Simulate packet drops - Add and distinguish regional download/upload tests

--- src/Tester.js
+++ src/Tester.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Footer, Home, About, History, Map } from "./components";
 import ReactSpeedometer from "react-d3-speedometer";
+import async from "async";
 var network = require('./networkSim');
 
 function Tester() {
@@ -12,11 +13,14 @@ function Tester() {
     var speed = .5;
     var stab = .3;
 
+    const [testName, setName] = useState("Download Test");
     const [value, setValue] = useState([0]);
-
+    const [startColor, setStartColor] = useState('#000000');
+    const [endColor, setEndColor] = useState('#59b1e3');
     //temporary set max value that can be generated
     const [maxValue, setMax] = useState([Math.ceil((1000 * speed + 1000 * speed * stab) / 10) * 10]);
     const [forceRender, changeForce] = useState([false]);
+
     const changeValue = () => {
         const test = network.getDownloadSpeed(speed, stab);
         console.log("Test value..." + test);
@@ -28,6 +32,18 @@ function Tester() {
         changeForce(false);
         setValue(test);
     }
+    const changeUploadValue = () => {
+        const test = network.getUploadSpeed(speed, stab);
+        console.log("Test value..." + test);
+        console.log("maxValue" + maxValue);
+        if (test > maxValue) {
+            changeForce(true);
+            setMax(Math.ceil(test / 10) * 10);
+        }
+        changeForce(false);
+        setValue(test);
+    }
+
     const startTest = () => {
         var pullRate = 30;
         var space = 200;
@@ -36,24 +52,44 @@ function Tester() {
         }
     }
 
+    const startUploadTest = () => {
+        setValue(0);
+        setName("Upload Test");
+        changeForce(true);
+        //TODO: Decide on color schemes, this is temp
+        setStartColor('#103319');
+        setEndColor('#15d445');
+        let space = 1000;
+        for(var i = 0; i< 50; i++){
+            setTimeout(() => {changeUploadValue();}, space * i);
+        }
+    }
+
     window.onload = function () {
+        // Not the cleanest way of handling this but because our test are running on a timer anyway
+        // we can just specify the delay amount so that tests happen one after another
+        // otherwise they overlap -- Chris
+        // TODO: value should reset to 0 after test completes
         setTimeout(() => { startTest(); }, 2000);
+        setTimeout(() => { startUploadTest(); }, 15000);
+        // TODO: Local vs Distance speed, Jitter, ping tests
     }
     return (
         <div className="App" className={"center"}>
             <center>
-                <h1> Download Test </h1>
+                <h1> {testName} </h1>
                 <ReactSpeedometer value={value}
                     minValue={0}
                     maxValue={maxValue}
                     forceRender={forceRender}
-                    currentValueText={'${value}Mbps'}
+                    currentValueText={'${value} Mbps'}
                     segments={1000}
                     maxSegmentLabels={10}
-                    startColor={'#000000'}
-                    endColor={'#59b1e3'}
-                    needleTransitionDuration={100}
+                    startColor={startColor}
+                    endColor={endColor}
                     textColor={'#ffffff'}
+                    //needleTransitionDuration={100}
+                    needleTransition={"easeElastic"}
                 />
             </center>
         </div>
