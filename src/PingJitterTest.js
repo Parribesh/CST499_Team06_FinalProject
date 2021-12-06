fab92201c75088f91452b58c1306ca9cf88990f3
Skye Ridge <mridge@csumb.edu>
2021-12-05 19:43:27 -0800

    added virginia ping and jitter test

--- src/PingJitterTest.js
+++ src/PingJitterTest.js
@@ -10,7 +10,10 @@ var network = require("./networkSim");
 function PingJitterTest() {
   const pingData = useRef([]);
   const jitterData = useRef([]);
+  const virginiaPingData = useRef([]);
+  const virginiaJitterData = useRef([]);
   const data = useRef([]);
+  const virginiaData = useRef([]);
   const isDone = useRef(false);
   const avgDown = useRef(0);
   const avgUp = useRef();
@@ -27,6 +30,7 @@ function PingJitterTest() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   let runningTotal = 0;
+
   const changeValue = () => {
     const test = network.getPing(0.1, 0.2);
     //use network.getPing(0.1, 0.4) for Virginia
@@ -34,6 +38,20 @@ function PingJitterTest() {
     data.current = [...pingData.current];
     runningTotal += test;
     updateAvg(Math.floor(runningTotal/pingData.current.length));
+    console.log(currentAvg);
+    //test changes each time changeValue is run
+    //this will force a re-render every time data is updated
+    forceUpdate(test);
+    // console.log(test);
+  };
+
+  const virginiaChangeValue = () => {
+    const test = network.getPing(0.1, 0.2);
+    //use network.getPing(0.1, 0.4) for Virginia
+    virginiaPingData.current = [...virginiaPingData.current, test];
+    data.current = [...virginiaPingData.current];
+    runningTotal += test;
+    updateAvg(Math.floor(runningTotal/virginiaPingData.current.length));
     console.log(currentAvg);
     //test changes each time changeValue is run
     //this will force a re-render every time data is updated
@@ -50,6 +68,17 @@ function PingJitterTest() {
     updateAvg(Math.floor(runningTotal/jitterData.current.length));
     forceUpdate(test);
   }
+
+  const virginiaJitterChangeValue = () =>{
+    const test = network.getJitter(0.1, 0.3);
+    //use network.getPing(0.1, 0.6) for Virginia
+    virginiaJitterData.current = [...virginiaJitterData.current, test];
+    data.current = [...virginiaJitterData.current];
+    runningTotal += test;
+    updateAvg(Math.floor(runningTotal/virginiaJitterData.current.length));
+    forceUpdate(test);
+  }
+
   const startTest = () => {
     //data.current.length = 0;
     var pullRate = 30;
@@ -123,6 +152,84 @@ function PingJitterTest() {
         currentValues.push(completionTime);
         sessionStorage.testCompletionTime = JSON.stringify(currentValues);
       }
+    }, 10500);
+  }
+
+  const startVirginiaPingTest = () => {
+    //data.current.length = 0;
+    var pullRate = 30;
+    var space = 200;
+    setRegion("Virginia");
+    updateTest("Ping ");
+    for (var i = 0; i < 15; i++) {
+      //setTimeout(() => { changeValue(); }, 200 * i);
+      setTimeout(() => {
+        virginiaChangeValue();
+      }, 1000 * i);
+    }
+    setTimeout(()=>{
+      if(sessionStorage.getItem('avgPing') != null){
+        //JSON.parse lets us pull the values out as whatever type
+        //We store it as an array so it'll always be an array
+        let currentValues = JSON.parse(sessionStorage.getItem('avgPing'));
+        currentValues.push(Math.floor(runningTotal/pingData.current.length));
+        //JSON.stringify spits it back out as a string for sessionStorage
+        sessionStorage.avgPing = JSON.stringify(currentValues);
+        //Throw a dummy value into avgUp to make it easier to display results
+        let old_values = JSON.parse(sessionStorage.getItem('avgJitter'));
+        old_values.push('');
+        sessionStorage.avgJitter = JSON.stringify(old_values);
+
+      }else{
+        //Make it an array before you add it to sessionStorage
+        let currentValues = [];
+        currentValues.push(Math.floor(runningTotal/pingData.current.length));
+        sessionStorage.avgPing = JSON.stringify(currentValues);
+        //We're gonna just throw a dummy value into avgUp as well to make it easier to display results
+        let dummyValues = [''];
+        sessionStorage.avgJitter = JSON.stringify(dummyValues);
+      }
+    }, 15500);
+  };
+
+  const startVirginiaJitterTest = () => {
+    updateTest("Jitter ");
+    for (var i = 0; i< 10; i++){
+      setTimeout(() => {
+        virginiaJitterChangeValue();
+      }, 1000 * i);
+    }
+    setTimeout(()=>{
+      if(JSON.parse(sessionStorage.getItem('avgJitter')).length > 0){
+        let currentValues =  JSON.parse(sessionStorage.getItem('avgJitter'));
+        //if(currentValues.get(currentValues.length) === null){
+        currentValues[currentValues.length - 1] = Math.floor(runningTotal/jitterData.current.length);
+        //}
+        //currentValues.push(Math.floor(avgUp/data.current.length));
+        sessionStorage.avgJitter = JSON.stringify(currentValues);
+
+      }else{
+        let currentValues = [];
+        currentValues.push(Math.floor(runningTotal/jitterData.current.length));
+        sessionStorage.avgJitter = JSON.stringify(currentValues);
+      }
+      //Get Current Time At End of Test
+      if(JSON.parse(sessionStorage.getItem('testCompletionTime')) != null){
+        let currentValues = JSON.parse(sessionStorage.getItem('testCompletionTime'));
+        let currentDate = new Date().toLocaleDateString();
+        let currentTime = new Date().toLocaleTimeString();
+        let completionTime = currentDate + " | " + currentTime;
+        currentValues.push(completionTime);
+        sessionStorage.testCompletionTime = JSON.stringify(currentValues);
+      }
+      else{
+        let currentValues = [];
+        let currentDate = new Date().toLocaleDateString();
+        let currentTime = new Date().toLocaleTimeString();
+        let completionTime = currentDate + " | " + currentTime;
+        currentValues.push(completionTime);
+        sessionStorage.testCompletionTime = JSON.stringify(currentValues);
+      }
       // show results modal view
       setTimeout( () => {
         let length = JSON.parse(sessionStorage.getItem('avgDown')).length - 1;
@@ -140,13 +247,28 @@ function PingJitterTest() {
     setTimeout(() => {
       startTest();
     }, 1500);
-    data.current = [];
     setTimeout( () => {
+      data.current = [];
       isDone.current = true;
       runningTotal = 0;
       updateAvg(0);
       startJitterTest();
     }, 17500)
+    setTimeout(() => {
+      
+      isDone.current = false;
+      data.current = [];
+      console.log("virginia ping");
+      startVirginiaPingTest();
+    }, 500+17500+13000);
+    setTimeout( () => {
+      data.current = [];
+      console.log("virginia jitter");
+      isDone.current = true;
+      runningTotal = 0;
+      updateAvg(0);
+      startVirginiaJitterTest();
+    }, 500+17500+13000+17000)
   }
 
   return (
